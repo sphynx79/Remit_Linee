c98d5fe06cbd3c4e4a7d32663cf08c6ce18a1726
ichelema <miboscol@gmail.com>
2017-09-22 12:37:47 +0200

    Sto creando il fatto che mi legge le remit e cerca il nome associato nelle mie transmission

--- app/models/transmission_model.rb
+++ app/models/transmission_model.rb
@@ -1,17 +1,30 @@
 require 'mongo'
-require 'date'
-require 'time'
-require 'csv'
+require 'chainable_methods'
+#https://jeffkreeftmeijer.com/2011/method-chaining-and-lazy-evaluation-in-ruby/
 
 HOUR_STEP   = (1.to_f/24)
-Mongo::Logger.logger.level = MONGO_LOG_LEVEL
+# Mongo::Logger.logger.level = env[:config].mongo["log_level"]
 # Mongo::Logger.logger.level = ::Logger::INFO
 
-class  RemitModel
+class TransmissionModel < Transmission::BaseModel
 
-  def initialize
 
+  def collection(collection: "transmission")
+    client[collection]
   end
 
-end
+  def all_document_from(collection: "transmission")
+    (client[collection]).
+      ᐅ(~:find, {}).
+      ᐅ(~:projection, '_id' => 1, 'properties.nome' => 1).
+      ᐅ(~:to_a)
+  end
 
+  # def all_document_from(collection: "transmission")
+  #   (client[collection])
+  #     .find({})
+  #     .projection('_id' => 1, 'properties.nome' => 1)
+  #     .to_a
+  # end
+
+end
--- lib/transmission.rb
+++ lib/transmission.rb
@@ -4,4 +4,5 @@ module Transmission
   autoload :Configuration, 'transmission/configuration'
   autoload :Application, 'transmission/application'
   autoload :BaseController, 'transmission/base_controller'
+  autoload :BaseModel, 'transmission/base_model'
 end
